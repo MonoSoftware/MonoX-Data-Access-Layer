ff4cf84bbc3c69f79e4f16d07cf2ccd60c6dae20
Kristijan Horvat <kristijan@mono.hr>
2014-01-02 12:50:35 +0100

    Updated version

--- DatabaseSpecific/AssemblyInfo.cs
+++ DatabaseSpecific/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Security;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("4.8.40.4594")]
+[assembly: AssemblyVersion("4.9.40.4845")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
